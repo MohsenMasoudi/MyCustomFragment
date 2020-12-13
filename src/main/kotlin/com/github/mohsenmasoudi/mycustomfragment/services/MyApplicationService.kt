c13040717fb11fea436eb5759d0db40f2c76b1ea
GitHub Action <action@github.com>
2020-12-13 06:41:09 +0000

    Template cleanup

---
+++ src/main/kotlin/com/github/mohsenmasoudi/mycustomfragment/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.mohsenmasoudi.mycustomfragment.services
+
+import com.github.mohsenmasoudi.mycustomfragment.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
--- src/main/kotlin/com/github/mohsenmasoudi/mycustomfragment/services/MyProjectService.kt
+++ src/main/kotlin/com/github/mohsenmasoudi/mycustomfragment/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.mohsenmasoudi.mycustomfragment.services
 
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.mohsenmasoudi.mycustomfragment.MyBundle
 
 class MyProjectService(project: Project) {
 
